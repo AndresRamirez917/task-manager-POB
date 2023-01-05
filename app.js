11846ccb10ec71c77553f7f63cda82bd4881f2b7
Andrés Ramirez <axy09179@gmail.com>
2023-01-05 14:58:23 -0500

    commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 class Task {
     constructor(name, day) {
-        (this.name = name), (this.day = day);
+        this.name = name,
+        this.day = day;
     }
 }
 
@@ -10,17 +11,41 @@ class UserInterface {
         const element = document.createElement("div");
         element.innerHTML = `
         <div class="card text-center mb-4">
-            <did class="card-body">
+            <div class="card-body">
                 <strong>Tarea</strong>: ${task.name}
                 <strong>Hora/Día</strong>: ${task.day}
+                <a href="#" class="btn btn-danger" name="delete">Eliminar</a>
             </div>
         </div> 
         `;
+        // EL LINK SE ESTILIZÓ COMO BOTÓN GRACIAS A LOS ESTILOS DE BOOSTRAP
+        taskList.appendChild(element);
+        this.resetForm();
     }
 
-    deleteTask() {}
+    resetForm(){
+        document.getElementById("task-form").reset();
+    }
 
-    showMessages() {}
+    deleteTask(element) {
+        if(element.name === "delete"){
+            element.parentElement.parentElement.parentElement.remove()
+            this.showMessages("Tarea eliminiada", "info")
+        }
+
+    }
+
+    showMessages(message, cssClass) {
+        const div = document.createElement("div");
+        div.className = `alert alert-${cssClass} mt-4`
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector(".container");
+        const app = document.querySelector("#App");
+        container.insertBefore(div, app)
+        setTimeOut = setTimeout(function(){
+            document.querySelector(".alert").remove()
+        }, 1000)
+    }
 }
 
 document.getElementById("task-form").addEventListener("submit", function (e) {
@@ -29,5 +54,20 @@ document.getElementById("task-form").addEventListener("submit", function (e) {
     console.log(taskName, taskTime);
     const task = new Task(taskName, taskTime);
     console.log(task);
+    const userInterface = new UserInterface();
+    if(taskName === "" || taskTime === ""){
+        // SIN ESTE RETURN MOSTRARÍA EL MENSAJE DE TAREA AGREGADA SATISFACTORIAMENTE
+        // CON EL RETURN OBLIGAMOS A QUE LA FUNCIÓN SE EJECUTE HASTA ESE PUNTO Y NO SIGA
+        return userInterface.showMessages("los campos no pueden estar vacíos", "danger")
+    }
+    // SE PUEDE LLAMAR TAMBIÉN DE ESTA MANERA
+    userInterface.addTask(task); 
+    userInterface.resetForm();
+    userInterface.showMessages("Tarea agregada satisfactoriamente", "success")
     e.preventDefault();
 });
+
+document.getElementById("task-list").addEventListener("click", function(e){
+    const userInterface = new UserInterface();
+    userInterface.deleteTask(e.target)
+})
